5f916ccb3cea1cbbf1e0b5cb9bdee25b0aff8058
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-03-17 18:04:52 +0530

    updated code structure

--- micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Login/SelectName.js
+++
@@ -1,8 +0,0 @@
-import { FormStep } from "@egovernments/digit-ui-components-core";
-import React from "react";
-
-const SelectName = ({ config, onSelect, t, isDisabled }) => {
-  return <FormStep config={config} onSelect={onSelect} t={t} isDisabled={isDisabled}></FormStep>;
-};
-
-export default SelectName;
