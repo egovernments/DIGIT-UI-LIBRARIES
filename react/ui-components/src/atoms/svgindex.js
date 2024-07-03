3bf9efc320ee832c62ef6414b654858424808af3
Swathi-eGov <swathi.chatrathi@egovernments.org>
2024-07-04 01:13:35 +0530

    added ProfileIcon svg

--- react/ui-components/src/atoms/svgindex.js
+++ react/ui-components/src/atoms/svgindex.js
@@ -622,7 +622,7 @@ const GetApp = ({ className }) => (
   </svg>
 );
 
-const HamburgerIcon = ({ className, styles, color = "#fdfdfd" }) => (
+const HamburgerIcon = ({ className, styles, color }) => (
   <svg
     style={{ ...styles }}
     width="24"
@@ -3128,7 +3128,7 @@ const PngFile = ({ className = "", fill = "none", style = {} }) => (
   </svg>
 );
 
-const DocUpload = ({ className = "", fill="none", styles }) => (
+const DocUpload = ({ className = "", fill = "none", styles }) => (
   <svg
     width="100"
     height="100"
@@ -3165,7 +3165,7 @@ const DocUpload = ({ className = "", fill="none", styles }) => (
   </svg>
 );
 
-const DocXlsxUpload = ({ className = "", fill="none", styles }) => (
+const DocXlsxUpload = ({ className = "", fill = "none", styles }) => (
   <svg
     width="100"
     height="100"
@@ -3229,7 +3229,7 @@ const DocXlsxUpload = ({ className = "", fill="none", styles }) => (
   </svg>
 );
 
-const DocdocUpload = ({ className = "", fill="none", styles }) => (
+const DocdocUpload = ({ className = "", fill = "none", styles }) => (
   <svg
     width="100"
     height="100"
@@ -3278,7 +3278,7 @@ const DocdocUpload = ({ className = "", fill="none", styles }) => (
   </svg>
 );
 
-const DocPdfUpload = ({ className = "", fill="none", styles }) => (
+const DocPdfUpload = ({ className = "", fill = "none", styles }) => (
   <svg
     width="100"
     height="100"
@@ -3325,9 +3325,35 @@ const DocPdfUpload = ({ className = "", fill="none", styles }) => (
   </svg>
 );
 
+const ProfileIcon = ({ className, styles,width,height }) => (
+  <svg
+    className={className}
+    style={{ ...styles }}
+    width={width || "32"}
+    height={height || "32"}
+    viewBox="0 0 64 64"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M64.1971 32C64.1971 42.1402 59.4661 51.1788 52.0842 57.0414C46.6012 61.3975 39.6545 64 32.0986 64C24.5426 64 17.596 61.3975 12.1129 57.0414C4.73099 51.1788 0 42.1402 0 32C0 14.3271 14.3712 0 32.0986 0C49.8259 0 64.1971 14.3271 64.1971 32Z"
+      fill="#E5E5E5"
+    />
+    <path
+      d="M32.0375 37.5803C39.2897 37.5803 45.1687 31.7193 45.1687 24.4893C45.1687 17.2594 39.2897 11.3984 32.0375 11.3984C24.7853 11.3984 18.9062 17.2594 18.9062 24.4893C18.9062 31.7193 24.7853 37.5803 32.0375 37.5803Z"
+      fill="#A6AFB2"
+    />
+    <path
+      d="M52.3384 57.0411C46.8554 61.3972 39.9088 63.9996 32.3528 63.9996C24.7969 63.9996 17.8502 61.3972 12.3672 57.0411C14.9628 48.4903 22.9285 42.2651 32.3528 42.2651C41.7771 42.2651 49.7428 48.4903 52.3384 57.0411Z"
+      fill="#A6AFB2"
+    />
+  </svg>
+);
+
 const ExpenditureIcon = BillsIcon;
 
 export {
+  ProfileIcon,
   AnnouncementIcon,
   ReceiptIcon,
   CreateEstimateIcon,
