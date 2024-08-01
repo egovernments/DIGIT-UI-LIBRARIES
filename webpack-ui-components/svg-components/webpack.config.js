7b53a5d81d0988d98df1e25be2d725cbba22e473
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-08-01 12:40:01 +0530

    taken the svg components

---
+++ webpack-ui-components/svg-components/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production", // Use "development" for faster rebuilds in watch mode
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    
+    globalObject: 'this',
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React',
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
