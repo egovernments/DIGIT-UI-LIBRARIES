75899d8e8ead94c7b22ca901227bf7ca61d0e8c8
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-08-02 14:18:05 +0530

    statble componnet without svg

--- webpack-ui-components/svg-components/webpack.config.js
+++ webpack-ui-components/svg-components/webpack.config.js
@@ -1,29 +1,38 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production", // Use "development" for faster rebuilds in watch mode
+  mode: "production",
   entry: "./src/index.js",
   output: {
-    filename: "index.js",
+    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    
-    globalObject: 'this',
+    library: {
+      name: "@egovernments/digit-ui-svg-components",
+      type: "umd",
+    },
+    globalObject: 'this', // Add this line to ensure compatibility in different environments
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: [".js"],
   },
   externals: {
     react: {
       commonjs: 'react',
       commonjs2: 'react',
-      amd: 'React',
+      amd: 'react',
       root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
     }
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
