18dfe131f143e3efd90a9261998be07e84d52922
Swathi-eGov <137176788+Swathi-eGov@users.noreply.github.com>
2024-08-27 20:31:28 +0530

    fixed mobilesidebar issue

--- react/ui-components/src/atoms/MobileSidebar.js
+++ react/ui-components/src/atoms/MobileSidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,Fragment} from "react";
 import PropTypes from "prop-types";
 import { SVG } from "./SVG";
 import { CustomSVG } from "./CustomSVG";
