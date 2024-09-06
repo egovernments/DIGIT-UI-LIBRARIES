21ed6fe46dc7dc91e6f4240b25e5c99c95ac1e8c
Swathi-eGov <swathi.chatrathi@egovernments.org>
2024-09-06 15:09:31 +0530

    updated stories

--- react/ui-components/src/molecules/stories/LandingPageCard.stories.js
+++ react/ui-components/src/molecules/stories/LandingPageCard.stories.js
@@ -141,12 +141,12 @@ WithoutLinks.args = {
   links: [],
 };
 
-export const LongText = Template.bind({});
-LongText.args = {
-  ...commonArgs,
-  moduleName:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-  metrics:longmetriclabel
-};
+// export const LongText = Template.bind({});
+// LongText.args = {
+//   ...commonArgs,
+//   moduleName:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+//   metrics:longmetriclabel
+// };
 
 export const WithCustomStyles = Template.bind({});
 WithCustomStyles.args = {
--- react/ui-components/src/molecules/stories/MenuCard.stories.js
+++ react/ui-components/src/molecules/stories/MenuCard.stories.js
@@ -38,27 +38,27 @@ WithoutDescription.args = {
 };
 
 // Default story for MenuCard
-export const WithLongMenuName = Template.bind({});
-WithLongMenuName.args = {
-  icon: "Article",
-  menuName:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-  description:
-    "Use this checklist to supervise the team formation for Registration & Distribution",
-  className: "",
-  styles: {},
-};
+// export const WithLongMenuName = Template.bind({});
+// WithLongMenuName.args = {
+//   icon: "Article",
+//   menuName:
+//     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+//   description:
+//     "Use this checklist to supervise the team formation for Registration & Distribution",
+//   className: "",
+//   styles: {},
+// };
 
 // MenuCard with a longer description
-export const WithLongDescription = Template.bind({});
-WithLongDescription.args = {
-  icon: "Article",
-  menuName: "Menu",
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-  className: "profile-class",
-  styles: {},
-};
+// export const WithLongDescription = Template.bind({});
+// WithLongDescription.args = {
+//   icon: "Article",
+//   menuName: "Menu",
+//   description:
+//     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+//   className: "profile-class",
+//   styles: {},
+// };
 
 // MenuCard with custom styles
 export const CustomStyles = Template.bind({});
