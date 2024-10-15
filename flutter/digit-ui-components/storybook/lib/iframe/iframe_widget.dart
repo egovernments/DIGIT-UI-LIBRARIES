cdd8de7cb3e942a22f1faf4ce33c319cca6b0a9a
rachana-egov <rachana.singh@egovernment.org>
2024-10-15 10:26:11 +0530

    modified iframe widget

--- flutter/digit-ui-components/storybook/lib/iframe/iframe_widget.dart
+++ flutter/digit-ui-components/storybook/lib/iframe/iframe_widget.dart
@@ -3,7 +3,7 @@ import 'dart:ui_web';
 import 'package:flutter/material.dart';
 
 class IframeWidget extends StatelessWidget {
-  final String url; // This is the full URL
+  final String url; // The full URL
   final double width;
   final double height;
 
@@ -16,14 +16,64 @@ class IframeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     // Register the viewType for the HTML iframe element
     final String viewType = 'iframeElement-$url';
     platformViewRegistry.registerViewFactory(
       viewType,
-          (int viewId) => IFrameElement()
-        ..src = url
-        ..style.border = 'none'
+          (int viewId) {
+        final IFrameElement iframe = IFrameElement()
+          ..src = url
+          ..style.border = 'none'
+          ..width = width.toString()
+          ..height = height.toString();
+
+        // Inject JavaScript to manipulate the iframe's content
+        iframe.onLoad.listen((event) {
+          iframe.contentWindow!.postMessage(
+            '''
+              (function() {
+                try {
+                  // Find the tab list element
+                  var tabList = document.querySelector('.group\\/tabs');
+                  
+                  if (tabList) {
+                    // Find all buttons inside the tab list
+                    var buttons = tabList.querySelectorAll('button');
+                    
+                    // If there are at least two buttons, switch the active class to the second button
+                    if (buttons.length > 1) {
+                      var firstButton = buttons[0];
+                      var secondButton = buttons[1];
+                      
+                      // Remove active class from the first button
+                      if (firstButton.classList.contains('active-tab')) {
+                        firstButton.classList.remove('active-tab');
+                      }
+                      
+                      // Add active class to the second button
+                      if (!secondButton.classList.contains('active-tab')) {
+                        secondButton.classList.add('active-tab');
+                      }
+                      
+                      // Optionally, trigger a click event on the second tab to ensure content loads
+                      secondButton.click();
+                    } else {
+                      console.log('Fallback: Only one button or no buttons found.');
+                    }
+                  } else {
+                    console.log('Fallback: Tab list not found. Showing default content.');
+                  }
+                } catch (error) {
+                  console.error('Error occurred while switching tabs:', error);
+                }
+              })();
+            ''',
+            '*', // Specify the target origin for security
+          );
+        });
+
+        return iframe;
+      },
     );
 
     return Center(
@@ -34,65 +84,3 @@ class IframeWidget extends StatelessWidget {
     );
   }
 }
-
-
-
-// import 'dart:html';
-// import 'dart:ui';
-// import 'package:flutter/material.dart';
-//
-// class IframeWidget extends StatelessWidget {
-//   final String url;
-//   final double width;
-//   final double height;
-//
-//   const IframeWidget({
-//     Key? key,
-//     required this.url,
-//     this.width = 800,
-//     this.height = 600,
-//   }) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     // Register the viewType for the HTML iframe element
-//     final String viewType = 'iframeElement-$url';
-//     platformViewRegistry.registerViewFactory(
-//       viewType,
-//           (int viewId) {
-//         final IFrameElement iframe = IFrameElement()
-//           ..src = url
-//           ..style.border = 'none'
-//           ..width = width.toString()
-//           ..height = height.toString();
-//
-//         // Inject custom JavaScript to manipulate the iframe's content
-//         iframe.onLoad.listen((event) {
-//           // Example: Hide an element with a specific class name
-//           iframe.contentWindow!.postMessage(
-//             '''
-//               (function() {
-//                 var elementToHide = document.querySelector('.class-to-hide');
-//                 if (elementToHide) {
-//                   elementToHide.style.display = 'none';
-//                 }
-//               })();
-//             ''',
-//             '*', // You can specify the target origin here for security
-//           );
-//         });
-//
-//         return iframe;
-//       },
-//     );
-//
-//     return Center(
-//       child: SizedBox(
-//         width: width,
-//         height: height,
-//         child: HtmlElementView(viewType: viewType),
-//       ),
-//     );
-//   }
-// }
-
