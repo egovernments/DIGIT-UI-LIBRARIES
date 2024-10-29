01684736c298e2d080b9d2b79642dad2efe1524a
Swathi-eGov <swathi.chatrathi@egovernments.org>
2024-10-29 11:11:16 +0530

    updated breadcrumb

--- react/ui-components/src/atoms/BreadCrumb.js
+++ react/ui-components/src/atoms/BreadCrumb.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState ,Fragment} from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
@@ -10,18 +10,18 @@ const BreadCrumb = (props) => {
     if (props?.maxItems && props?.crumbs.length > props?.maxItems && !expanded) {
       const startCrumbs = props.crumbs.slice(0, props?.itemsBeforeCollapse || Math.ceil(props.maxItems / 2));
       const endCrumbs = props.crumbs.slice(
-        -1 * (props?.itemsAfterCollapse || Math.floor(props.maxItems / 2)) 
+        -1 * (props?.itemsAfterCollapse || Math.floor(props.maxItems / 2))
       );
-      
+
       let updatedCrumbs = startCrumbs.concat(
         [{ show: true, content: props?.expandText || "..." }],
         endCrumbs
       );
       setCrumbsToDisplay(updatedCrumbs);
-    }else {
+    } else {
       setCrumbsToDisplay([...props.crumbs]);
     }
-  }, [props.crumbs, props.maxItems, expanded,props.itemsBeforeCollapse,props.itemsAfterCollapse,props?.expandText]);
+  }, [props.crumbs, props.maxItems, expanded, props.itemsBeforeCollapse, props.itemsAfterCollapse, props?.expandText]);
 
   function handleRedirect(path) {
     const host = window.location.origin; // Dynamically get the base URL
@@ -29,8 +29,13 @@ const BreadCrumb = (props) => {
   }
 
   function isLast(index) {
-    let validcrumb = crumbsToDisplay?.filter((ob) => ob?.show === true)
-    return validcrumb?.findIndex((ob) => ob?.path === crumbsToDisplay?.[index]?.path) === validcrumb?.length - 1;
+    // Filter crumbs to only include those that are displayed
+    let validCrumbs = crumbsToDisplay?.filter((crumb) => crumb?.show === true);
+    // Check if the current crumb is the last one in the validCrumbs list
+    return validCrumbs?.findIndex((crumb) =>
+      crumb?.internalLink === crumbsToDisplay[index]?.internalLink ||
+      crumb?.externalLink === crumbsToDisplay[index]?.externalLink
+    ) === validCrumbs?.length - 1;
   }
 
   const handleCrumbClick = () => {
@@ -39,9 +44,7 @@ const BreadCrumb = (props) => {
 
   return (
     <ol
-      className={`digit-bread-crumb ${
-        props?.className ? props?.className : ""
-      }`}
+      className={`digit-bread-crumb ${props?.className ? props?.className : ""}`}
       style={props?.style}
     >
       {crumbsToDisplay?.map((crumb, ci) => {
@@ -51,39 +54,39 @@ const BreadCrumb = (props) => {
             <li
               key={ci}
               style={props?.itemStyle}
-              className={`digit-bread-crumb--item ${"back-crumb-item"}`}
+              className="digit-bread-crumb--item back-crumb-item"
             >
               <span onClick={() => window.history.back()}>{crumb.content}</span>
             </li>
           );
+
         return (
-          <>
+          <Fragment>
             <li
-              key={ci}
+            key={ci}
               style={props?.itemStyle}
               className="digit-bread-crumb--item"
             >
-              {isLast(ci) || !crumb?.path ? (
+              {isLast(ci) || (!crumb?.internalLink && !crumb?.externalLink) ? (
                 <span
-                  className={`digit-bread-crumb-content ${
-                    isLast(ci) ? "current" : "default"
-                  }`}
+                  className={`digit-bread-crumb-content ${isLast(ci) ? "current" : "default"}`}
                   style={props?.spanStyle}
-                  onClick={(crumb.content === "..."  || crumb.content === props?.expandText)? handleCrumbClick : null}
+                  onClick={(crumb.content === "..." || crumb.content === props?.expandText) ? handleCrumbClick : null}
                 >
                   {crumb?.icon && crumb.icon}
                   {crumb.content}
                 </span>
-              ) : crumb?.externalPath ? (
-                <Link 
-                className="digit-bread-crumb-content"
-                onClick={() => handleRedirect(crumb?.path)}>
+              ) : crumb?.externalLink ? (
+                <Link
+                  className="digit-bread-crumb-content"
+                  onClick={() => handleRedirect(crumb?.externalLink)}
+                >
                   {crumb?.icon && crumb.icon}
                   {crumb.content}
                 </Link>
               ) : (
                 <Link
-                  to={{ pathname: crumb.path, state: { count: crumb?.count } }}
+                  to={{ pathname: crumb.internalLink, state: { count: crumb?.count } }}
                   className="digit-bread-crumb-content"
                 >
                   {crumb?.icon && crumb.icon}
@@ -96,7 +99,7 @@ const BreadCrumb = (props) => {
                 </div>
               )}
             </li>
-          </>
+          </Fragment>
         );
       })}
     </ol>
@@ -109,10 +112,10 @@ BreadCrumb.propTypes = {
   style: PropTypes.object,
   spanStyle: PropTypes.object,
   customSeparator: PropTypes.element,
-  maxItems:PropTypes.number,
-  itemsAfterCollapse:PropTypes.number,
-  itemsBeforeCollapse:PropTypes.number,
-  expandText:PropTypes.string
+  maxItems: PropTypes.number,
+  itemsAfterCollapse: PropTypes.number,
+  itemsBeforeCollapse: PropTypes.number,
+  expandText: PropTypes.string
 };
 
 BreadCrumb.defaultProps = {
