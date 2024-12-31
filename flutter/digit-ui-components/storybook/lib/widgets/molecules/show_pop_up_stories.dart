a19d12f04c7970986e4ada63a2cfb1b933d45cbd
rachana-egov <rachana.singh@egovernment.org>
2024-12-31 15:11:20 +0530

    updated pop up stories

--- flutter/digit-ui-components/storybook/lib/widgets/molecules/show_pop_up_stories.dart
+++ flutter/digit-ui-components/storybook/lib/widgets/molecules/show_pop_up_stories.dart
@@ -10,65 +10,95 @@ import 'package:flutter/material.dart';
 List<Story> showPopUPStories() {
   return [
     Story(
-      name: 'Molecule/Show Pop Up/Default',
-      builder: (context) => ElevatedButton(
-        onPressed: () {
-          showCustomPopup(
-            context: context,
-            builder: (popupContext) => Popup(
-              title: "Simple Popup",
-              onOutsideTap: () {
-                Navigator.of(popupContext).pop();
-              },
-              subHeading: "This is a subheading",
-              description:
-                  "This popup demonstrates a simple popup with a title and description.",
-              type: PopUpType.simple,
-              actions: [
-                DigitButton(
-                  label: 'Submit',
-                  onPressed: () {
-                    Navigator.of(popupContext)
-                        .pop(); // Use popupContext to close the popup
-                  },
-                  type: DigitButtonType.primary,
-                  size: DigitButtonSize.large,
-                ),
-              ],
-            ),
-          );
-        },
-        child: const Text("Show Simple Popup"),
-      ),
+      name: 'Molecule/Show Pop Up/Basic',
+      builder: (context){
+
+         bool withIcon = context.knobs.boolean(label: 'Icon for heading', initial: false);
+         bool description = context.knobs.boolean(label: 'Description', initial: false);
+         bool actions = context.knobs.boolean(label: 'Actions', initial: false);
+         bool close = context.knobs.boolean(label: 'Close', initial: false);
+
+        return DigitButton(
+          type: DigitButtonType.secondary,
+          size: DigitButtonSize.large,
+          label: 'Click to open popup',
+          onPressed: () {
+            showCustomPopup(
+              context: context,
+              builder: (popupContext) => Popup(
+                title: "Simple Popup",
+                onOutsideTap: () {
+                  Navigator.of(popupContext).pop();
+                },
+                titleIcon: withIcon ? Icon(Icons.article, size: 32,) : null,
+                onCrossTap: close ? (){
+                  Navigator.of(popupContext).pop();
+                } : null,
+                subHeading: "This is a subheading",
+                description:
+                description ? "This popup demonstrates a simple popup with a title and description." : null,
+                type: PopUpType.simple,
+                actions: actions ? [
+                  DigitButton(
+                    label: 'Submit',
+                    onPressed: () {
+                      Navigator.of(popupContext)
+                          .pop(); // Use popupContext to close the popup
+                    },
+                    type: DigitButtonType.primary,
+                    size: DigitButtonSize.large,
+                  ),
+                ] : null ,
+              ),
+            );
+          },
+        );
+      },
     ),
     Story(
       name: 'Molecule/Show Pop Up/Alert',
-      builder: (context) => ElevatedButton(
-        onPressed: () {
-          showCustomPopup(
-            context: context,
-            builder: (popupContext) => Popup(
-              title: 'Alert!',
-              onOutsideTap: () {
-                Navigator.of(popupContext).pop();
-              },
-              subHeading:
-                  'Please contact the administrator if you have any query.',
-              type: PopUpType.alert,
-              actions: [
-                DigitButton(
-                    label: 'OK',
+      builder: (context){
+        bool withIcon = context.knobs.boolean(label: 'Icon for heading', initial: false);
+        bool description = context.knobs.boolean(label: 'Description', initial: false);
+        bool actions = context.knobs.boolean(label: 'Actions', initial: false);
+        bool close = context.knobs.boolean(label: 'Close', initial: false);
+
+        return DigitButton(
+          type: DigitButtonType.secondary,
+          size: DigitButtonSize.large,
+          label: 'Click to open alert popup',
+          onPressed: () {
+            showCustomPopup(
+              context: context,
+              builder: (popupContext) => Popup(
+                title: "Alert Popup",
+                titleIcon: withIcon ? Icon(Icons.article, size: 32,) : null,
+                onCrossTap: close ? (){
+                  Navigator.of(popupContext).pop();
+                } : null,
+                onOutsideTap: () {
+                  Navigator.of(popupContext).pop();
+                },
+                subHeading: "This is a subheading",
+                description:
+                description ? "This popup demonstrates a alert popup with a title and description." : null,
+                type: PopUpType.alert,
+                actions: actions ? [
+                  DigitButton(
+                    label: 'Submit',
                     onPressed: () {
-                      Navigator.of(popupContext).pop();
+                      Navigator.of(popupContext)
+                          .pop(); // Use popupContext to close the popup
                     },
                     type: DigitButtonType.primary,
-                    size: DigitButtonSize.large)
-              ],
-            ),
-          );
-        },
-        child: const Text("Show Simple Popup"),
-      ),
+                    size: DigitButtonSize.large,
+                  ),
+                ] : null,
+              ),
+            );
+          },
+        );
+      },
     ),
   ];
 }
