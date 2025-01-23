6bdddbac949a47a0ed687e27e20e0166b4f57117
Swathi-eGov <swathi.chatrathi@egovernments.org>
2025-01-23 10:50:04 +0530

    updated versions and other fixes

--- react/modules/sample/src/pages/employee/index.js
+++ react/modules/sample/src/pages/employee/index.js
@@ -3,15 +3,16 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { Route, Switch } from "react-router-dom";
 import Sample from "./Sample";
+import SampleSearch from "./SampleSearch";
 
 const ProjectBreadCrumb = ({ location }) => {
   const { t } = useTranslation();
   const crumbs = [
-    {
-      path: `/${window?.contextPath}/employee`,
-      content: t("HOME"),
-      show: true,
-    },
+    // {
+    //   path: `/${window?.contextPath}/employee`,
+    //   content: t("HOME"),
+    //   show: true,
+    // },
     {
       path: `/${window?.contextPath}/employee`,
       content: t(location.pathname.split("/").pop()),
@@ -31,6 +32,7 @@ const App = ({ path, stateCode, userType, tenants }) => {
           <ProjectBreadCrumb location={location} />
         </React.Fragment>
         <PrivateRoute path={`${path}/components`} component={() => <Sample></Sample>} />
+        <PrivateRoute path={`${path}/search`} component={() => <SampleSearch></SampleSearch>} />
         </AppContainer>
     </Switch>
   );
