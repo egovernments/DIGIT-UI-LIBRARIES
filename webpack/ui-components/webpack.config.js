6625c1ab10086f7083618fcd768c6b0cb405ae94
Piyush Raj <piyushraj473@gmail.com>
2025-04-10 16:54:48 +0530

    ui-components cleanup

--- webpack/ui-components/webpack.config.js
+++ webpack/ui-components/webpack.config.js
@@ -4,32 +4,32 @@ module.exports = {
   mode: "production",
   entry: "./src/index.js",
   output: {
-    filename: "[name].[contenthash].js", // Good for caching
-    chunkFilename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"), // 'build' instead of 'dist'
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
     library: {
       name: "@egovernments/digit-ui-components",
       type: "umd",
     },
-    globalObject: "this", // Makes UMD work in both browser and Node
-    // publicPath: Removed, not needed for libraries
+    globalObject: 'this', // Add this line to ensure compatibility in different environments
   },
   resolve: {
     extensions: [".js"],
   },
   externals: {
     react: {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "react",
-      root: "React",
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
     },
-    "react-dom": {
-      commonjs: "react-dom",
-      commonjs2: "react-dom",
-      amd: "react-dom",
-      root: "ReactDOM",
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
     },
+    'react-i18next': 'react-i18next',
+    'react-router-dom': 'react-router-dom'
   },
   module: {
     rules: [
@@ -44,29 +44,5 @@ module.exports = {
         },
       },
     ],
-  },
-  optimization: {
-    runtimeChunk: "single",
-    moduleIds: "hashed",
-    splitChunks: {
-      chunks: "all",
-      minSize: 30000,
-      maxSize: 250000,
-      minChunks: 1,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
-        },
-        common: {
-          test: /[\\/]src[\\/]/,
-          name: "common",
-          minChunks: 2,
-        },
-      },
-    },
   },
 };
