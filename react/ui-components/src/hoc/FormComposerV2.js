2151f850215bf93f5e08f119cfda72a6af3ef821
Bhavya-egov <bhavya.mangal@egovernments.org>
2025-04-15 15:03:48 +0530

    fix

--- react/ui-components/src/hoc/FormComposerV2.js
+++ react/ui-components/src/hoc/FormComposerV2.js
@@ -12,6 +12,7 @@ import Footer from "../atoms/Footer";
 import LabelFieldPair from "../atoms/LabelFieldPair";
 import HorizontalNav from "../atoms/HorizontalNav";
 import { SubmitBar, Toast } from "../atoms";
+import Button from "../atoms";
 
 // import Fields from "./Fields";    //This is a field selector pickup from formcomposer
 import FieldController from "./FieldController";
