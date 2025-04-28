9a40e0a421eeeb108543b111434aa3f1c96aeb6a
KOLAGANI NAVYA <141171461+KolaganiNavya@users.noreply.github.com>
2025-04-28 10:30:39 +0530

    Update FormComposerV2.js

--- react/ui-components/src/hoc/FormComposerV2.js
+++ react/ui-components/src/hoc/FormComposerV2.js
@@ -396,7 +396,7 @@ export const FormComposer = (props) => {
   const schemaValid = useMemo(() => {
     const validate = ajv.compile(schema);
     const valid = validate(props.config);
-    console.log(validate.errors);
+    //console.log(validate.errors);
     if (!valid) {
       const combinedMessage = validate.errors
         ?.map(err => {
