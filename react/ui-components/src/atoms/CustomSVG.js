71cb62bc77f3746b7211c19cd5db0a30b234c8dc
NabeelAyubee <nayubi7@gmail.com>
2025-07-16 15:41:13 +0530

    fix

--- react/ui-components/src/atoms/CustomSVG.js
+++ react/ui-components/src/atoms/CustomSVG.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { PRIMARY_COLOR } from "../hoc/app-config-screen-builder/app-config-utils/constants";
 
 const CreateEstimateIcon = ({ className, style = {}, fill = "#C84C0E",width = '18',height ='18' }) => (
   <svg
@@ -436,11 +435,11 @@ const DustbinIcon = ({fill="#C84C0E" ,width="14",height="18"}) => (
   </svg>
 );
 
-export const DustbinIconNew = () => (
+export const DustbinIconNew = ({ fill="#C84C0E" }) => (
   <svg width="12" height="16" viewBox="0 0 12 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M0.999837 13.8333C0.999837 14.75 1.74984 15.5 2.6665 15.5L9.33317 15.5C10.2498 15.5 10.9998 14.75 10.9998 13.8333L10.9998 3.83333L0.999837 3.83333L0.999837 13.8333ZM11.8332 1.33333L8.9165 1.33333L8.08317 0.5L3.9165 0.5L3.08317 1.33333L0.166504 1.33333L0.166504 3L11.8332 3V1.33333Z"
-      fill={PRIMARY_COLOR}
+      fill={fill}
     />
   </svg>
 );
