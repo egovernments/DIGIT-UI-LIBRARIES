d9d384220285161d2e26a82840bf40cf6ffd476c
KolaganiNavya-egov <navya.kolagani@egovernments.org>
2025-07-21 10:45:51 +0530

    recent component changes

--- react/ui-components/src/hoc/UploadAndDownloadDocumentHandler.js
+++ react/ui-components/src/hoc/UploadAndDownloadDocumentHandler.js
@@ -32,27 +32,19 @@ const UploadAndDownloadDocumentHandler = ({
   let moduleName = `${module?.toLowerCase()}.${service?.toLowerCase()}`;
   const { serviceCode, applicationNumber:applicationNo = "" } = Digit.Hooks.useQueryParams();
 
-  const requestCriteria = {
-    url: "/egov-mdms-service/v1/_search",
+  const serviceconfigrequestCriteria = {
+    url: "/egov-mdms-service/v2/_search",
     body: {
       MdmsCriteria: {
-        "tenantId": tenantId,
-        "moduleDetails": [
-            {
-                "moduleName": "DigitStudio",
-                "masterDetails": [
-                    {
-                        "name": "DocumentConfig2"
-                    }
-                ]
+        tenantId: tenantId,
+        schemaCode: "Studio.ServiceConfiguration",
+            filters:{
+              module:module
             }
-        ]
-    },
-    },
-    changeQueryName: "documentConfig",
-  };
-
-  const { isLoading, data } = Digit.Hooks.useCustomAPIHook(requestCriteria);
+          },
+        },
+      };
+ const { isLoading, data:serviceconfig } = Digit.Hooks.useCustomAPIHook(serviceconfigrequestCriteria);
 
   const downloadPdf = (blob, fileName) => {
     if (window.mSewaApp && window.mSewaApp.isMsewaApp() && window.mSewaApp.downloadBase64File) {
@@ -139,9 +131,10 @@ const UploadAndDownloadDocumentHandler = ({
   };
   
 
-  let docData = data ? data?.MdmsRes?.DigitStudio?.DocumentConfig2?.filter((ob) => ob?.module.toLowerCase() === moduleName)?.[0]?.actions : [];
+  //let docData = data ? data?.MdmsRes?.DigitStudio?.DocumentConfig2?.filter((ob) => ob?.module.toLowerCase() === moduleName)?.[0]?.actions : [];
+  let docData = serviceconfig ? serviceconfig?.mdms?.filter((ob) => ob?.uniqueIdentifier.toLowerCase() === moduleName)?.[0]?.data?.documents?.[0].actions : [];
 
-  const docConfig = docData?.filter((item) => item?.action === "APPLY" || item?.action === "apply")?.[0];
+  const docConfig = docData?.filter((item) => item?.action === ("APPLY" || "apply"))?.[0];
   const updatedDocuments = docConfig?.documents?.flatMap((doc) => {
     if (doc.templatePDFKey || doc.templatedownloadURL) {
       // Return both original and a modified copy with cleared template keys
@@ -242,7 +235,7 @@ const UploadAndDownloadDocumentHandler = ({
                     )}
                   </div>
                 )}
-                
+
             <div className="digit-field">
               {item?.showTextInput && (
                 <TextInput
