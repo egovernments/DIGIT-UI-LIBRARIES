704dce2cbf6be1112ddce0b4f7bf24c5df15770f
Swathi-eGov <swathi.chatrathi@egovernments.org>
2025-08-09 18:55:51 +0530

    webpack changes

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -7,10 +7,14 @@ module.exports = {
   // mode: 'development',
   entry: "./src/index.js",
   devtool: "none",
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   module: {
     rules: [
       {
-        test: /\.js|\.jsx$/,
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
         use: ["babel-loader"],
       },
       {
