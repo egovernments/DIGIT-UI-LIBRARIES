8a6493f2e7f9c1ca35272f2abd601c27f5581b2a
Swathi-eGov <swathi.chatrathi@egovernments.org>
2025-08-11 10:38:27 +0530

    webpack change test

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -15,7 +15,12 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
       },
       {
         test: /\.css$/i,
