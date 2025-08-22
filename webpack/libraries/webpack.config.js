1c03c55739ead5615fb03a1ea2d2f42b855d75ac
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2025-08-22 18:52:30 +0530

    optimise

--- webpack/libraries/webpack.config.js
+++ webpack/libraries/webpack.config.js
@@ -1,49 +1,61 @@
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    library: {
-      name: "@egovernments/digit-ui-libraries",
-      type: "umd",
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    entry: "./src/index.js",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+      library: {
+        name: "@egovernments/digit-ui-libraries",
+        type: "umd",
+      },
+      globalObject: 'this',
     },
-    globalObject: 'this', // Add this line to ensure compatibility in different environments
-  },
-  resolve: {
-    extensions: [".js"],
-  },
-  externals: {
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      root: 'React',
+    resolve: {
+      extensions: [".js"],
     },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
-      root: 'ReactDOM',
+    externals: {
+      react: {
+        commonjs: 'react',
+        commonjs2: 'react',
+        amd: 'react',
+        root: 'React',
+      },
+      'react-dom': {
+        commonjs: 'react-dom',
+        commonjs2: 'react-dom',
+        amd: 'react-dom',
+        root: 'ReactDOM',
+      },
+      'react-i18next': 'react-i18next',
+      'react-router-dom': 'react-router-dom',
+      "@tanstack/react-query": "@tanstack/react-query"
     },
-    'react-i18next': 'react-i18next',
-    'react-router-dom': 'react-router-dom',
-    "@tanstack/react-query": "@tanstack/react-query"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
           },
         },
-      },
-    ],
-  },
+      ],
+    },
+    optimization: {
+      minimize: isProduction,
+      sideEffects: false
+    },
+    performance: {
+      hints: isProduction ? 'warning' : false
+    }
+  };
 };
