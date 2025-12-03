979119eea1eac3589158627c551e419df7f6ef2d
NabeelAyubee <nayubi7@gmail.com>
2025-12-03 12:18:19 +0530

    Tag missing props animation fix

--- react/ui-components/src/atoms/Tag.js
+++ react/ui-components/src/atoms/Tag.js
@@ -3,15 +3,42 @@ import PropTypes from "prop-types";
 import { SVG } from "./SVG";
 import { Colors } from "../constants/colors/colorconstants";
 import { iconRender } from "../utils/iconRender";
+import Animation from "./Animation";
+import theLoaderPrimary2 from "../constants/animations/theLoaderPrimary2.json";
 
-const Tag = ({ className, label, style, type, icon, stroke, showIcon, labelStyle, onClick, alignment, iconClassName, iconColor }) => {
+const Tag = ({
+  className,
+  label,
+  style,
+  type,
+  icon,
+  stroke,
+  showIcon,
+  labelStyle,
+  onClick,
+  alignment,
+  iconClassName,
+  iconColor,
+  loader,
+  animationStyles,
+}) => {
   const MonochromeIconColor = Colors.lightTheme.primary[2];
   const SuccessIconColor = Colors.lightTheme.alert.success;
   const ErrorIconColor = Colors.lightTheme.alert.error;
   const WarningIconColor = Colors.lightTheme.alert.warning;
 
-  let iconToShow;
-  if (icon) {
+  const commonProps = {
+    loop: animationStyles?.noLoop !== true,
+    autoplay: animationStyles?.noAutoplay !== true,
+    width: animationStyles?.width || "1rem",
+    height: animationStyles?.height || "1rem",
+    animationData: theLoaderPrimary2,
+  };
+
+  let iconToShow = null;
+  if (loader) {
+    iconToShow = <Animation {...commonProps} />;
+  } else if (icon) {
     iconToShow = iconRender(icon, iconColor || MonochromeIconColor, "1rem", "1rem", `digit-tag-customIcon ${iconClassName}`);
   } else {
     switch (type) {
@@ -56,6 +83,7 @@ Tag.propTypes = {
   label: PropTypes.string.isRequired,
   style: PropTypes.object,
   stroke: PropTypes.bool,
+  loader: PropTypes.bool,
   type: PropTypes.string,
 };
 
@@ -69,6 +97,7 @@ Tag.defaultProps = {
   alignment: "center",
   iconColor: "",
   iconClassName: "",
+  loader: false,
 };
 
 export default Tag;
